2c84a41d4878cd8b9edae53acc5226e1c9b05cd5
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-08-06 16:24:58 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const container = document.querySelector('.gallery')
+// console.log(container);
+function createMarkup(arr) {
+return arr.map(({ preview, original, description }) => `
+<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt=""${description}" />
+</a>
+</li>`).join('')
+}
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+container.addEventListener('click', handlerGalleryClick)
+function handlerGalleryClick(evt) {
+    evt.preventDefault()
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+const picture = evt.target.dataset.sourse;
+const currentItem = evt.target.closest('gallery__item')
+// console.log(evt,target);
+// console.log(currentItem);
+
+console.log(SimpleLigthbox);
+
+
+ }
+
 
 console.log(galleryItems);
+
+var lightbox = new SimpleLigthbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+});
+
