ff4ddb651a8803f431b9e08824e57d9df109533b
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-08-06 16:31:14 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -13,28 +13,25 @@ return arr.map(({ preview, original, description }) => `
 
 container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
 
-container.addEventListener('click', handlerGalleryClick)
-function handlerGalleryClick(evt) {
-    evt.preventDefault()
-    if (!evt.target.classList.contains('gallery__image')) {
-        return;
-    }
-const picture = evt.target.dataset.sourse;
-const currentItem = evt.target.closest('gallery__item')
+// container.addEventListener('click', handlerGalleryClick)
+// function handlerGalleryClick(evt) {
+//     evt.preventDefault()
+//     if (!evt.target.classList.contains('gallery__image')) {
+//         return;
+//     }
+// const picture = evt.target.dataset.sourse;
+// const currentItem = evt.target.closest('gallery__item')
 // console.log(evt,target);
 // console.log(currentItem);
 
+//  }
 
-
-
- }
-
-
-console.log(galleryItems);
-
-var lightbox = new SimpleLigthbox('.gallery a', {
+new SimpleLigthbox('.gallery a', {
     captionsData: 'alt',
     captionsDelay: 250,
 });
+console.log(galleryItems);
+
+
 
 // console.log(SimpleLigthbox);
